826982026374b437c8a3acd3cb4bef4671bc1443
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-01-18 00:08:34 +0530

    Create DELETION AT LAST IN CIRCULAR  LINKED LIST .C

---
+++ DELETION AT LAST IN CIRCULAR  LINKED LIST .C
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node *next;
+};
+
+// traversal
+void circular_linked_list(struct node *head)
+{
+    struct node *ptr = head;
+    do
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    } while (ptr != head);
+}
+
+// deletion function
+struct node * deleteatend(struct node * head){
+    struct node * ptr = head->next;
+    struct node * p =head ;
+    while(ptr->next!=head){
+        ptr=ptr->next;
+        p=p->next;
+
+    }
+
+    p->next=head;
+    free(ptr);
+    return head;
+
+};
+
+int main(){
+    struct node *head;
+    struct node *second;
+    struct node *third;
+    struct node *fourth;
+    struct node *fifth;
+
+    head = (struct node *)malloc(sizeof(struct node));
+    second = (struct node *)malloc(sizeof(struct node));
+    third = (struct node *)malloc(sizeof(struct node));
+    fourth = (struct node *)malloc(sizeof(struct node));
+    fifth = (struct node *)malloc(sizeof(struct node));
+
+    // link nodes to each other
+    head->data = 11;
+    head->next = second;
+
+    second->data = 8;
+    second->next = third;
+
+    third->data = 52;
+    third->next = fourth;
+
+    fourth->data = 69;
+    fourth->next = fifth;
+
+    fifth->data = 55;
+    fifth->next = head;
+    printf("linked list before deletion\n");
+    circular_linked_list(head);
+    head = deleteatend(head);
+    printf("linked list after deletion\n");
+    circular_linked_list(head);
+
+}
