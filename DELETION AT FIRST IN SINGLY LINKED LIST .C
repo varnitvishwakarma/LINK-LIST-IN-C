d7d159706e3e30d9c2960a0a5a6c9b205c0fc796
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-01-17 16:34:10 +0530

    Create DELETION AT FIRST IN SINGLY LINKED LIST .C

---
+++ DELETION AT FIRST IN SINGLY LINKED LIST .C
@@ -0,0 +1,60 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+    int data;
+    struct node * next;
+};
+
+void linklist(struct node*ptr){
+    while (ptr!=NULL){
+        printf("%d\n", ptr->data);
+        ptr = ptr->next;
+    }
+};
+
+struct node * deletefirstnode(struct node * head){
+    struct node * ptr = head;
+    head = head->next;
+    free(ptr);
+    return head;
+
+};
+
+int main(){
+
+struct node * head;
+struct node * second;
+struct node * third;
+struct node * fourth;
+struct node * fifth;
+
+head = (struct node * )malloc(sizeof(struct node));
+second = (struct node * )malloc(sizeof(struct node));
+third =(struct node * )malloc(sizeof(struct node));
+fourth = (struct node * )malloc(sizeof(struct node));
+fifth = (struct node * )malloc(sizeof(struct node));
+
+//link nodes to each other
+head->data = 10;
+head->next = second;
+
+second->data = 20;
+second->next = third;
+
+third->data = 30;
+third->next = fourth;
+
+fourth->data = 40;
+fourth->next = fifth;
+
+fifth->data = 50;
+fifth->next = NULL;
+
+
+printf("link list before deletion\n");
+linklist(head);
+head = deletefirstnode(head);
+printf("link list after deletion\n");
+linklist(head);
+}
