c6313430ab205c155c8f8ec17b56afbec3124033
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-01-18 15:52:45 +0530

    Create INSERTION AT BETWEEN IN DOUBLY LINKED LIST .C

---
+++ INSERTION AT BETWEEN IN DOUBLY LINKED LIST .C
@@ -0,0 +1,83 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+  int data;
+  struct node *pre;
+  struct node * next;
+
+};
+
+
+void doubly(struct node * head){
+    struct node * ptr = head;
+    do{
+        printf("%d\n", ptr->data);
+        ptr=ptr->next;
+    }
+    while(ptr!=NULL);
+}
+
+// insertion function
+struct node * insertionatbetween(struct node * head , int data , int index){
+      struct node * q =(struct node *)malloc(sizeof(struct node));
+      q->data=data;
+      struct node * p = head;
+      int i =0;
+      while(i!=index-1){
+        p=p->next;
+        i++;
+      }
+      q->next=p->next;
+      q->pre=p;
+      p->next=q;
+      return head;
+
+
+}
+
+int main (){
+
+struct node * head;
+struct node * sec;
+struct node * third;
+struct node * fourth;
+struct node * fifth;
+
+head = (struct node *)malloc(sizeof(struct node ));
+sec = (struct node *)malloc(sizeof(struct node ));
+third = (struct node *)malloc(sizeof(struct node ));
+fourth = (struct node *)malloc(sizeof(struct node ));
+fifth = (struct node *)malloc(sizeof(struct node ));
+
+
+// link nodes to each other
+head->data=1;
+head->pre=NULL;
+head->next=sec;
+
+sec->data=2;
+sec->pre=head;
+sec->next=third;
+
+third->data=3;
+third->pre=sec;
+third->next=fourth;
+
+fourth->data=4;
+fourth->pre=third;
+fourth->next=fifth;
+
+fifth->data=5;
+fifth->pre=fourth;
+fifth->next=NULL;
+
+
+// function call
+printf("LINKED LIST BEFORE INSERTION\n");
+doubly(head);
+head = insertionatbetween(head , 8, 3);
+printf("LINKED LIST AFTER INSERTION\n");
+doubly(head);
+
+}
