2fd245aaca2fba8040b44cad4427522ffe94cb5e
varnit <120304453+varnitvishwakarma@users.noreply.github.com>
2023-01-18 16:39:41 +0530

    Create INSERTION AT END IN DOUBLY LINKED LIST .C

---
+++ INSERTION AT END IN DOUBLY LINKED LIST .C
@@ -0,0 +1,81 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node {
+  int data;
+  struct node *pre;
+  struct node * next;
+
+};
+
+
+void doubly(struct node * head){
+    struct node * ptr = head;
+    do{
+        printf("%d\n", ptr->data);
+        ptr=ptr->next;
+    }
+    while(ptr!=NULL);
+}
+
+// insertion function
+struct node * insertionatend(struct node * head , int data){
+      struct node * ptr =(struct node *)malloc(sizeof(struct node));
+      ptr->data=data;
+      struct node * p = head;
+      while(p->next!=NULL){
+        p=p->next;
+      }
+      p->next=ptr;
+      ptr->pre=p;
+      ptr->next=NULL;
+      return head;
+
+
+}
+
+int main (){
+
+struct node * head;
+struct node * sec;
+struct node * third;
+struct node * fourth;
+struct node * fifth;
+
+head = (struct node *)malloc(sizeof(struct node ));
+sec = (struct node *)malloc(sizeof(struct node ));
+third = (struct node *)malloc(sizeof(struct node ));
+fourth = (struct node *)malloc(sizeof(struct node ));
+fifth = (struct node *)malloc(sizeof(struct node ));
+
+
+// link nodes to each other
+head->data=1;
+head->pre=NULL;
+head->next=sec;
+
+sec->data=2;
+sec->pre=head;
+sec->next=third;
+
+third->data=3;
+third->pre=sec;
+third->next=fourth;
+
+fourth->data=4;
+fourth->pre=third;
+fourth->next=fifth;
+
+fifth->data=5;
+fifth->pre=fourth;
+fifth->next=NULL;
+
+
+// function call
+printf("LINKED LIST BEFORE INSERTION\n");
+doubly(head);
+head = insertionatend(head ,44);
+printf("LINKED LIST AFTER INSERTION\n");
+doubly(head);
+
+}
